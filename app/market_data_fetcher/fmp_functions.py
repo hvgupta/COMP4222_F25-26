200dc95839847faae5075218e9fa292016ed366d
hvgupta <hvgupta@outlook.in>
2025-11-01 17:36:51 +0800

    changing the file name, and fixing the endpoint name

--- app/market_data_fetcher/fmp_functions.py
+++ app/market_data_fetcher/fmp_functions.py
@@ -7,13 +7,14 @@ dotenv.load_dotenv(override=True)
 fmp_base_url = os.getenv("FMP_BASE_URL")
 
 
-def call_fmp_api(function_name: str, **kwargs):
+def call_fmp_api(function_name: str, params: dict = {}):
     fmp_api_key = os.getenv("FMP_API_KEY")
     if not fmp_api_key:
         raise ValueError("FMP_API_KEY not found in environment variables.")
+    params["apikey"] = fmp_api_key
     response = requests.get(
         f"{fmp_base_url}/{function_name}",
-        params={**kwargs, "apikey": fmp_api_key}
+        params=params
     )
     response.raise_for_status()
     return response.json()
@@ -23,8 +24,10 @@ def call_fmp_api(function_name: str, **kwargs):
 
 def get_historical_data_for_company(ticker_symbol: str, start_date: str, end_date: str):
     return call_fmp_api(
-        "historical-price-eod",
-        symbol=ticker_symbol,
-        from_=start_date,
-        to=end_date
+        "historical-price-eod/light",
+        {
+            "symbol": ticker_symbol,
+            "from": start_date,
+            "to": end_date
+        }
     )
