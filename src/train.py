ab2245b7aef990dcef1d1f4e0a9a09be532ed4f6
hvgupta <hvgupta@outlook.in>
2025-11-17 21:54:15 +0800

    reducing the epoach time

--- src/train.py
+++ src/train.py
@@ -122,7 +122,7 @@ def save_model(model: TwoTowerSAGE, filepath: str):
 
 
 async def main():
-    model = await train_model(WINDOW_SIZE, CORRELATION_THRESHOLD, num_epoch=500)
+    model = await train_model(WINDOW_SIZE, CORRELATION_THRESHOLD, num_epoch=30)
     if model is None:
         raise ValueError("Something is wrong here")
 
